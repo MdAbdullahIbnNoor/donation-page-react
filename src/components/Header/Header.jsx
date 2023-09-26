6948124110471b37f347a0fd5c2fca947e800348
MdAbdullahIbnNoor <ab.89noor@gmail.com>
2023-09-26 23:21:57 +0600

    little more changes in navbar section

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
         <div className=" bg-base-100 py-5 flex items-center bg-transparent md:max-w-screen-md lg:max-w-screen-2xl mx-auto gap-5 flex-col md:flex-row">
             <div className="flex-1">
                 <Link to="/" className="w-full mx-auto">
-                    <img className="md:w-2/5" src="../images/Logo.png" alt="" /></Link>
+                    <img className="lg:w-1/5 md:w-2/5" src="../images/Logo.png" alt="" /></Link>
             </div>
             <div className="flex-none">
                 <ul className="menu menu-horizontal px-1 text-lg font-semibold">
